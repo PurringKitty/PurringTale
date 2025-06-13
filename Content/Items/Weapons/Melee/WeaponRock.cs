06003abc69277aff0b53c23c9461009ebc0a37a0
The Catxylst <deboned.cat@gmail.com>
2025-06-12 22:02:19 -0400

    Update WeaponRock.cs

--- Content/Items/Weapons/Melee/WeaponRock.cs
+++ Content/Items/Weapons/Melee/WeaponRock.cs
@@ -22,21 +22,8 @@ public class WeaponRock : ModItem
             Item.value = Item.sellPrice(gold: 100);
             Item.rare = ItemRarityID.Master;
             Item.UseSound = SoundID.Item1;
-            Item.autoReuse = true;
             Item.master = true;
             Item.masterOnly = true;
-            Item.noMelee = true;
-            Item.shootsEveryUse = true;
             Item.autoReuse = true;
         }
-
-    public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-    {
-        float adjustedItemScale = player.GetAdjustedItemScale(Item);
-        Projectile.NewProjectile(source, player.MountedCenter, new Vector2(player.direction, 0f), type, damage, knockback, player.whoAmI, player.direction * player.gravDir, player.itemAnimationMax, adjustedItemScale);
-        NetMessage.SendData(MessageID.PlayerControls, -1, -1, null, player.whoAmI);
-
-        return base.Shoot(player, source, position, velocity, type, damage, knockback);
-    }
-
 }
